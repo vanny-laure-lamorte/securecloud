11fa0923fef55e7a5d98546ef2d99d2b78c046da
Vanny Laure Lamorte <146710289+vanny-laure-lamorte@users.noreply.github.com>
2025-10-28 10:13:38 +0100

    B/v1.0/auth service (#2)
    * ✨ feat(env):  allow reading secrets from env files
    • add dotenv.h file
    • apdate 'auth-service' and 'auth-service' CmakeLists
    • create file 'EnLoader' to handle environment variables
    
    * Correction PR #1
    
    * ✨ feat(authentification): add a new user in db.
    
    * 🐛 fix(authServiceRoutes): remove cout.

---
+++ services/auth-service/repository/UserRepository.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../../../common/db/DbConnection.h"
+#include <drogon/drogon.h>
+using namespace drogon;
+
+class UserRepository
+{
+public:
+    explicit UserRepository(DbConnection &dbConnection)
+        : db_(dbConnection) {};
+
+    /*
+    * Add a user
+    */
+    void addUser(const std::string &email,
+                 const std::string &hashedPassword,
+                 int userId);
+
+private:
+    DbConnection &db_;
+};
---
+++ services/auth-service/routes/AuthServiceRoutes.cpp
@@ -0,0 +1,115 @@
+#include "AuthServiceRoutes.h"
+#include "../utils/HashUtils.h"
+
+AuthServiceRoutes::AuthServiceRoutes(DbConnection &db_)
+    : userRepository_(db_) {}
+
+void AuthServiceRoutes::registerRoutes()
+{
+    using namespace drogon;
+
+    app().registerHandler(
+        "/auth/ping",
+        [](const HttpRequestPtr &,
+           std::function<void(const HttpResponsePtr &)> &&cb)
+        {
+            auto resp = HttpResponse::newHttpResponse();
+            resp->setContentTypeCode(CT_TEXT_PLAIN);
+            resp->setBody("accessing login service");
+            cb(resp);
+        },
+        {Get});
+
+    app().registerHandler(
+        "/auth/login",
+        [](const HttpRequestPtr &req,
+           std::function<void(const HttpResponsePtr &)> &&cb)
+        {
+            auto json = req->getJsonObject();
+            if (!json || !json->isMember("email") || !json->isMember("password"))
+            {
+                Json::Value respJson;
+                respJson["error"] = "Missing email or password";
+
+                auto resp = HttpResponse::newHttpJsonResponse(respJson);
+                resp->setStatusCode(k400BadRequest);
+                cb(resp);
+                return;
+            }
+
+            std::string email = (*json)["email"].asString();
+            std::string password = (*json)["password"].asString();
+
+            Json::Value respJson;
+            if (email == "test@test.com" && password == "123456")
+            {
+                respJson["message"] = "Login successful";
+                auto resp = HttpResponse::newHttpJsonResponse(respJson);
+                resp->setStatusCode(k200OK);
+                cb(resp);
+            }
+            else
+            {
+                respJson["error"] = "Invalid email or password";
+                auto resp = HttpResponse::newHttpJsonResponse(respJson);
+                resp->setStatusCode(k401Unauthorized);
+                cb(resp);
+            }
+        },
+        {Post});
+
+    app().registerHandler(
+    "/auth/register",
+    [this](const HttpRequestPtr &req,
+           std::function<void(const HttpResponsePtr &)> &&cb)
+    {
+        auto json = req->getJsonObject();
+
+        if (!json || !json->isMember("email") || !json->isMember("password") || !json->isMember("user_id"))
+        {
+            Json::Value respJson;
+            respJson["status"] = "error";
+            respJson["message"] = "Missing email, password, or user_id";
+
+            auto resp = HttpResponse::newHttpJsonResponse(respJson);
+            resp->setStatusCode(k400BadRequest);
+            cb(resp);
+            return;
+        }
+
+        std::string email = (*json)["email"].asString();
+        std::string password = (*json)["password"].asString();
+        int userId = (*json)["user_id"].asInt();
+
+        std::string salt = HashUtils::generateSalt(10);
+        std::string hashedPassword = HashUtils::hashPassword(password, salt);
+
+        try
+        {
+            userRepository_.addUser(email, hashedPassword, userId);
+
+            Json::Value respJson;
+            respJson["status"] = "success";
+            respJson["message"] = "User registered successfully";
+            respJson["email"] = email;
+
+            auto resp = HttpResponse::newHttpJsonResponse(respJson);
+            resp->setStatusCode(k201Created);
+            cb(resp);
+
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Error registering user: " << e.what() << std::endl;
+
+            Json::Value respJson;
+            respJson["status"] = "error";
+            respJson["message"] = std::string("Registration failed: ") + e.what();
+
+            auto resp = HttpResponse::newHttpJsonResponse(respJson);
+            resp->setStatusCode(k500InternalServerError);
+            cb(resp);
+        }
+    },
+    {Post});
+}
