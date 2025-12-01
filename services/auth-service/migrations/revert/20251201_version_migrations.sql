846bd5f1c6e4f7724149d016e6723693b5522c87
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-12-01 14:37:33 +0100

    🐛 fix(auth-service): rename migration file name.

--- services/auth-service/migrations/revert/20251201_version_migrations.sql
+++
@@ -1,7 +0,0 @@
--- Revert auth:20251201_version_migrations from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
---
+++ services/auth-service/migrations/verify/20251201142023_version_migrations.sql
@@ -0,0 +1,7 @@
+-- Verify auth:20251201142023_version_migrations on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
