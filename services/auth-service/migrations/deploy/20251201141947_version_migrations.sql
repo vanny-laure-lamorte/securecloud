b0908c6d4cb13524d16c2eea2db1dffdd046a5c1
Vanny Laure Lamorte <146710289+vanny-laure-lamorte@users.noreply.github.com>
2025-12-04 15:07:33 +0100

    B/v1.1/sqitch (#7)
    * ⚡️ feat(auth-service): set up sqitch.
    
    * 🐛 fix(auth-service): rename migration file name.
    
    * 🔥fix(migrations): remove migration file.

---
+++ services/auth-service/migrations/deploy/20251201141947_version_migrations.sql
@@ -0,0 +1,7 @@
+-- Deploy auth:20251201141947_version_migrations to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ services/auth-service/migrations/revert/20251201141947_version_migrations.sql
@@ -0,0 +1,7 @@
+-- Revert auth:20251201141947_version_migrations from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
