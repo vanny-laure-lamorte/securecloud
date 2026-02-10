36bf57663f2d99388172187db9b793ac8af98f3f
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2026-02-10 19:59:44 +0100

    ✨ feat(commands): create custom commands

---
+++ scripts/sqitch.ps1
@@ -0,0 +1,93 @@
+param(
+    [string]$Command,
+    [string]$Name,
+    [string]$Note
+)
+
+$RootDir = Split-Path $PSScriptRoot -Parent
+$MigrationsDir = Join-Path $RootDir "services\auth-service\migrations"
+$SecretsFile = Join-Path $RootDir "secrets\auth_db_password.txt"
+
+$SqitchImage = "sqitch/sqitch:latest"
+$Network = "securecloud_default"
+$DbUrl = "db:pg://postgres@auth-db:5432/auth_service"
+
+# TODO: init sqitch, generate migration folder if not exists, etc.
+# Write-Host "  sqitch:revert                  Revert migrations"
+# Write-Host "  sqitch:verify                  Verify migrations"
+
+if (-not $Command -or $Command -eq "sqitch") {
+    Show-SqitchHelp
+    exit 0
+}
+
+switch ($Command) {
+
+    "sqitch:version" {
+        Write-Host "[ Checking Sqitch version... ]"
+        docker run --rm -it $SqitchImage --version
+    }
+
+    "sqitch:generate:audit" {
+        Write-Host "[ Generating audit DB migration file... ]"
+        docker run --rm -it `
+        -v "${PWD}\services\audit-service/migrations:/repo" `
+        -w /repo `
+        sqitch/sqitch:latest add "$(date +%Y%m%d%H%M%S)_version_migrations" -n "Create table x"
+    }
+
+    "sqitch:generate:auth" {
+        Write-Host "[ Generating auth DB migration file... ]"
+        docker run --rm -it `
+        -v "${PWD}\services\auth-service/migrations:/repo" `
+        -w /repo `
+        sqitch/sqitch:latest add "$(date +%Y%m%d%H%M%S)_version_migrations" -n "Create table x"
+    }
+
+    "sqitch:generate:messaging" {
+        Write-Host "[ Generating messaging DB migration file... ]"
+        docker run --rm -it `
+        docker run --rm -it `
+        -v "${PWD}\services\messaging-service/migrations:/repo" `
+        -w /repo `
+        sqitch/sqitch:latest add "$(date +%Y%m%d%H%M%S)_version_migrations" -n "Create table x"
+    }
+
+    "sqitch:deploy:audit" {
+        Write-Host "[ Deploying audit DB migrations... ]"
+        $pw = Get-Content ./secrets/audit_db_password.txt -Raw
+        docker run --rm -it `
+        -v "${PWD}/services/audit-service/migrations:/repo" `
+        --network securecloud_default `
+        -e PGPASSWORD="$pw" `
+        sqitch/sqitch:latest `
+        deploy db:pg://postgres@auth-db:5432/audit_service --chdir /repo/
+    }
+
+    "sqitch:deploy:auth" {
+        Write-Host "[ Deploying auth DB migrations... ]"
+        $pw = Get-Content ./secrets/auth_db_password.txt -Raw
+        docker run --rm -it `
+        -v "${PWD}/services/auth-service/migrations:/repo" `
+        --network securecloud_default `
+        -e PGPASSWORD="$pw" `
+        sqitch/sqitch:latest `
+        deploy db:pg://postgres@auth-db:5432/auth_service --chdir /repo/
+    }
+
+    "sqitch:deploy:messaging" {
+        Write-Host "[ Deploying messaging DB migrations... ]"
+        $pw = Get-Content ./secrets/messaging_db_password.txt -Raw
+        docker run --rm -it `
+        -v "${PWD}/services/messaging-service/migrations:/repo" `
+        --network securecloud_default `
+        -e PGPASSWORD="$pw" `
+        sqitch/sqitch:latest `
+        deploy db:pg://postgres@auth-db:5432/messaging_service --chdir /repo/
+    }
+
+    default {
+        Write-Host "Unknown command: $Command"
+        exit 1
+    }
+}
--- scripts/translate.ps1
+++ scripts/translate.ps1
@@ -9,34 +9,17 @@ $ClientSrc = Join-Path $RootDir "client\src\main.cpp"
 $I18nPath  = Join-Path $RootDir "common\i18n"
 $BuildDir = Join-Path $RootDir "build"
 
-function Show-TranslateHelp {
-    Write-Host ""
-    Write-Host "Translation"
-    Write-Host ""
-    Write-Host "  translate:scan       Scan translation keys"
-    Write-Host "  translate:update     Compile translation files (from .ts to .qm)"
-    Write-Host "  translate:migrate    Build client with translations"
-    Write-Host ""
-}
-
-if (-not $Command -or $Command -eq "translate") {
-    Show-TranslateHelp
-    exit 0
-}
-
 $env:PATH = "C:\Qt\6.7.1\mingw_64\bin;" + $env:PATH
 
 switch ($Command) {
 
-    # Scan
     "translate:scan" {
-        Write-Host "[1/3] Scanning translation keys"
+        Write-Host "[1/3 Building client... ]"
 
         if (-not (Test-Path $ClientSrc)) {
             Write-Host "Source file not found: $ClientSrc"
             exit 1
         }
-
         lupdate $ClientSrc `
             -ts "$I18nPath\fr_FR.ts" `
                 "$I18nPath\en_US.ts" `
@@ -45,9 +28,8 @@ switch ($Command) {
         Write-Host "Scan completed."
     }
 
-    # Update
     "translate:update" {
-        Write-Host "[2/3] Compiling translation files"
+        Write-Host "[2/3 Compiling translation files... ] "
 
         lrelease `
             "$I18nPath\fr_FR.ts" `
@@ -57,18 +39,14 @@ switch ($Command) {
         Write-Host " Translations compiled."
     }
 
-    # Compile
     "translate:compile" {
-        Write-Host "[3/3] Building client"
-
+        Write-Host "[3/3  Building client... ]"
         cmake --build $BuildDir --target client
-
         Write-Host "Build completed."
     }
 
     default {
         Write-Host "Unknown translation command: $Command"
-        Show-TranslateHelp
         exit 1
     }
 }
