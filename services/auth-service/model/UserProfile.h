21c0f6784d7312cf516b27b25cf8483f669b0a92
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2026-02-28 11:27:38 +0100

    🐛 fix(language): fix language selection on login page

--- services/auth-service/model/UserProfile.h
+++ services/auth-service/model/UserProfile.h
@@ -9,6 +9,16 @@ struct UserProfile {
     std::string firstName;
     std::string lastName;
     std::string email;
+    std::time_t createdAt;
+    std::time_t updatedAt;
+    std::string roleName;
+    std::string roleDescription;
+    std::string jwt;
+};
+
+struct UserSession {
+    std::string username;
+    std::string email;
     std::string jwt;
     std::time_t lastSeen;
     std::time_t createdAt;
